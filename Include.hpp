01c21e5233df91c23bed15a9450b757f5df6dea8
[Graham Donaldson] <[grahambo437@gmail.com]>
2020-07-30 14:19:25 -0700

    Added FindPattern

--- Include.hpp
+++ Include.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <Windows.h>			//allows use of windows api
 #include <iostream>				//allows use of printing to console
+#include <Psapi.h>				//header used by Process Status API (initially for patternscan.hpp)
 
 #pragma warning(disable:4996)	//disables warning printing console
 
