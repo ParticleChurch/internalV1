6d4fa7cd696c7a2b47ee8f7d89ab92c064515b2a
unknown <gabe.millikan@geoffandcindy.com>
2020-07-30 16:39:01 -0700

    free console on eject

--- Hooks.cpp
+++ Hooks.cpp
@@ -45,6 +45,7 @@ void H::UnHook()
 {
 	d3d9.RestoreOriginal();
 	D3dInit = false; //for wndproc... haven't found better solution
+	FreeConsole();
 }
 
 long __stdcall H::EndSceneHook(IDirect3DDevice9* device)
