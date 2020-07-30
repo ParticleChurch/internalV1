3c6d807f0f7a935c5c1685e21da3912ca04d4eb7
[Graham Donaldson] <[grahambo437@gmail.com]>
2020-07-30 13:10:10 -0700

    Allocated console, and added Include.h

---
+++ Include.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+
+#pragma warning(disable:4996)
--- dllmain.cpp
+++ dllmain.cpp
@@ -1,4 +1,14 @@
-#include <Windows.h>
+#include "Include.h"
+
+void Init()
+{
+    //alloc console
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+
+
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -8,6 +18,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, LPTHREAD_START_ROUTINE(Init), 0, 0, 0);
+        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
