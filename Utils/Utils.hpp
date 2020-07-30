20bf2931a699d1ad9ca346a9095a598cabe658e7
[Graham Donaldson] <[grahambo437@gmail.com]>
2020-07-30 14:05:13 -0700

    Added files for patternscanning and macros

--- Utils/Utils.hpp
+++ Utils/Utils.hpp
@@ -1,2 +1,4 @@
 #pragma once
-#include "GetVFunc.hpp"
+#include "GetVFunc.hpp"			//for calling virtual functions inside of csgo classes
+#include "Macro.hpp"			//for abusive macro calls (mainly having to do with math)
+#include "PatternScan.hpp"		//for scanning in memory for specific functions
