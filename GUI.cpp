96b3e8d72618cf6166f9f4751d7478b554030244
unknown <gabe.millikan@geoffandcindy.com>
2020-07-30 21:12:12 -0700

    more gui

--- GUI.cpp
+++ GUI.cpp
@@ -5,12 +5,12 @@ bool GUI::Main()
 	bool KillGui = false;
 
 	// LoginWindow
-	ImGui::SetNextWindowSize(ImVec2(400, 200));
-	ImGui::Begin("Hack", 0, ImGuiWindowFlags_NoResize);
+	ImGui::SetNextWindowSize(ImVec2(400 + 40, 200 + 20 + 40));
+	ImGui::Begin("Hack", 0, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar);
 
 	if (Config::UserInfo.IsAuthenticated) {
 		// Unload button
-		ImGui::SetCursorPos(ImVec2(150, 30));
+		ImGui::SetCursorPos(ImVec2(20 + 150, 20 + 20));
 		if (ImGui::Button("Unload", ImVec2(100, 40)))
 			KillGui = true;
 
@@ -19,15 +19,20 @@ bool GUI::Main()
 	}
 	else {
 		// Unload button
-		ImGui::SetCursorPos(ImVec2(5, 200 - 27));
-		if (ImGui::Button("Unload", ImVec2(80, 20)))
+		ImGui::SetCursorPos(ImVec2(10, 220));
+		if (ImGui::Button("Unload", ImVec2(70, 30)))
+			KillGui = true;
+
+		// Login button
+		ImGui::SetCursorPos(ImVec2(360, 220));
+		if (ImGui::Button("Login", ImVec2(70, 30)))
 			KillGui = true;
 
 		// Login Form
-		ImGui::SetCursorPos(ImVec2(200 - 40, 30));
-		ImGui::Text("Login");
-
-		ImGui::InputText("", Config::UserInfo.Email, 256);
+		static char Password[64] = "";
+		ImGui::SetCursorPos(ImVec2(20, 30));
+		ImGui::InputText("Email:", Config::UserInfo.Email, 256);
+		ImGui::InputText("Password:", Password, 64);
 	}
 
 	ImGui::End();
