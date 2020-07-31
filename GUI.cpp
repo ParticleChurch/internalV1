c7ac89f41b3a6572c28ff854d82aa82bc377320e
unknown <gabe.millikan@geoffandcindy.com>
2020-07-30 21:35:19 -0700

    Login Form Complete

--- GUI.cpp
+++ GUI.cpp
@@ -5,10 +5,11 @@ bool GUI::Main()
 	bool KillGui = false;
 
 	// LoginWindow
-	ImGui::SetNextWindowSize(ImVec2(400 + 40, 200 + 20 + 40));
-	ImGui::Begin("Hack", 0, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar);
 
 	if (Config::UserInfo.IsAuthenticated) {
+		ImGui::SetNextWindowSize(ImVec2(20 + 50, 200 + 20 + 40));
+		ImGui::Begin("Hack", 0, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar);
+
 		// Unload button
 		ImGui::SetCursorPos(ImVec2(20 + 150, 20 + 20));
 		if (ImGui::Button("Unload", ImVec2(100, 40)))
@@ -18,25 +19,30 @@ bool GUI::Main()
 		std::cout << "xd" << std::endl;
 	}
 	else {
-		// Unload button
-		ImGui::SetCursorPos(ImVec2(10, 220));
-		if (ImGui::Button("Unload", ImVec2(70, 30)))
-			KillGui = true;
-
-		// Login button
-		ImGui::SetCursorPos(ImVec2(360, 220));
-		if (ImGui::Button("Login", ImVec2(70, 30)))
-			KillGui = true;
+		ImGui::SetNextWindowSize(ImVec2(440, 114));
+		ImGui::Begin("Login", 0, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar);
 
 		// Login Form
-		ImGui::SetCursorPos(ImVec2(41, 30));
+		ImGui::SetCursorPos(ImVec2(59, 30));
 		ImGui::Text("Email:"); ImGui::SameLine();
 		ImGui::InputText("", Config::UserInfo.Email, 256);
 
 		static char Password[64] = "";
-		ImGui::SetCursorPos(ImVec2(20, 55));
+		ImGui::SetCursorPos(ImVec2(38, 55));
 		ImGui::Text("Password:"); ImGui::SameLine();
-		ImGui::InputText("", Password, 256);
+		ImGui::InputText("", Password, 64);
+
+
+		// Unload button
+		ImGui::SetCursorPos(ImVec2(10, 83));
+		if (ImGui::Button("Eject", ImVec2(70, 20)))
+			KillGui = true;
+
+
+		// Login button
+		ImGui::SetCursorPos(ImVec2(360, 83));
+		if (ImGui::Button("Login", ImVec2(70, 20)))
+			KillGui = true;
 	}
 
 	ImGui::End();
