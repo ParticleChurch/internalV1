420d4fa3e2c89e82590695e022b024b6c4af67cb
[Graham Donaldson] <[grahambo437@gmail.com]>
2020-07-30 17:07:07 -0700

    added color

---
+++ Valve/Custom/Color.h
@@ -0,0 +1,43 @@
+#pragma once
+class Color
+{
+public:
+
+	unsigned char color[4];
+
+	Color(float r, float g, float b, float a = 255.00f)
+	{
+		color[0] = r;
+		color[1] = g;
+		color[2] = b;
+		color[3] = a;
+	}
+
+	float* Base()
+	{
+		float clr[3];
+
+		clr[0] = color[0] / 255.0f;
+		clr[1] = color[1] / 255.0f;
+		clr[2] = color[2] / 255.0f;
+
+		return &clr[0];
+	}
+
+	float r() {
+		return color[0];
+	}
+
+	float g() {
+		return color[1];
+	}
+
+	float b() {
+		return color[2];
+	}
+
+	float a() {
+		return color[3];
+	}
+
+};
