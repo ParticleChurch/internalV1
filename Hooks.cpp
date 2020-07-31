9afefdd2ea7291d619531c34cab45eb7d134f876
[Graham Donaldson] <[grahambo437@gmail.com]>
2020-07-30 23:15:04 -0700

    added painttraverse, and snaplines

--- Hooks.cpp
+++ Hooks.cpp
@@ -8,10 +8,13 @@ namespace H
 	VMTManager d3d9VMT;
 	VMTManager clientVMT;
 	VMTManager clientmodeVMT;
+	VMTManager surfaceVMT;
+	VMTManager panelVMT;
 
 	EndScene oEndScene;
 	Reset oReset;
 	CreateMove oCreateMove;
+	PaintTraverse oPaintTraverse;
 	
 	//GUI Vars
 	bool D3dInit = false;
@@ -36,6 +39,8 @@ void H::Init()
 	d3d9VMT.Initialise((DWORD*)D3d9Device);
 	clientVMT.Initialise((DWORD*)I::client);
 	clientmodeVMT.Initialise((DWORD*)I::clientmode);
+	surfaceVMT.Initialise((DWORD*)I::surface);
+	panelVMT.Initialise((DWORD*)I::panel);
 
 	std::cout << "Endscene...";
 	oEndScene = (EndScene)d3d9VMT.HookMethod((DWORD)&EndSceneHook, 42);
@@ -48,11 +53,16 @@ void H::Init()
 	std::cout << "CreateMove...";
 	oCreateMove = (CreateMove)clientmodeVMT.HookMethod((DWORD)&CreateMoveHook, 24);
 	std::cout << "Success!" << std::endl;
+
+	std::cout << "PaintTraverse...";
+	oPaintTraverse = (PaintTraverse)panelVMT.HookMethod((DWORD)&PaintTraverseHook, 41);
+	std::cout << "Success!" << std::endl;
 }
 
 void H::UnHook()
 {
 	SetWindowLongPtr(CSGOWindow, GWL_WNDPROC, (LONG_PTR)oWndProc);
+	panelVMT.RestoreOriginal();
 	d3d9VMT.RestoreOriginal();
 	clientmodeVMT.RestoreOriginal();
 	clientVMT.RestoreOriginal();
@@ -117,5 +127,63 @@ bool __stdcall H::CreateMoveHook(float flInputSampleTime, CUserCmd* cmd)
 	}
 	oCreateMove(I::clientmode, flInputSampleTime, cmd);
 	return true; //silent aim on false (only for client)
+}
+
+bool WorldToScreen(Vec& in, Vec& out)
+{
+	auto matrix = I::engine->WorldToScreenMatrix().c;
+
+	float w = matrix[3][0] * in.x + matrix[3][1] * in.y + matrix[3][2] * in.z + matrix[3][3];
+
+	if (w > 0.001f) {
+		static int width = -1;
+		static int height = -1;
+		I::surface->GetScreenSize(width, height);
+		out.x = width / 2 * (1 + (matrix[0][0] * in.x + matrix[0][1] * in.y + matrix[0][2] * in.z + matrix[0][3]) / w);
+		out.y = height / 2 * (1 - (matrix[1][0] * in.x + matrix[1][1] * in.y + matrix[1][2] * in.z + matrix[1][3]) / w);
+		out.z = 0.0f;
+		return true;
+	}
+	return false;
+}
+
+void __stdcall H::PaintTraverseHook(int vguiID, bool force, bool allowForcing)
+{
+	oPaintTraverse(I::panel, vguiID, force, allowForcing);
+	if (I::panel && strcmp(I::panel->GetName(vguiID), "MatSystemTopPanel") == 0) {
+		Entity* Localplayer = I::entitylist->GetClientEntity(I::engine->GetLocalPlayer());
+		if (!Localplayer || !I::engine->IsInGame() || !(Localplayer->GetHealth() > 0))
+			return;
+		
+		I::surface->DrawSetColor(Color(255.f, 255.f, 255.f, 255.f));
+		for (int i = 0; i < I::entitylist->GetHighestEntityIndex(); i++)
+		{
+			if (i == I::engine->GetLocalPlayer())
+				continue;
+
+			Entity* Ent = I::entitylist->GetClientEntity(i);
+			if (!Ent)
+				continue;
+
+			player_info_t PlayerInfo;
+			if (!I::engine->GetPlayerInfo(i, &PlayerInfo))
+				continue;
+
+			Vec Screen;
+			Vec Loc = Ent->GetBonePos(8);
+
+			
+			if (WorldToScreen(Loc, Screen))
+			{
+				int xSize = 0;
+				int ySize = 0;
+
+				I::engine->GetScreenSize(xSize, ySize);
+				I::surface->DrawLine(xSize/2, ySize/2, Screen.x, Screen.y);
+			}
+			
+		}
+		
+	}
 }
 
--- Hooks.hpp
+++ Hooks.hpp
@@ -9,10 +9,12 @@ namespace H
 	typedef long(__stdcall* Reset)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
 	typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
 	typedef void(__thiscall* CreateMove)(void*, float, CUserCmd*);
+	typedef void(__thiscall* PaintTraverse)(void*, int, bool, bool);
 
 	//our functions
 	extern long __stdcall EndSceneHook(IDirect3DDevice9* device);
 	extern long __stdcall ResetHook(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* pPresentationParameters);
 	extern LRESULT __stdcall WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 	extern bool __stdcall CreateMoveHook(float flInputSampleTime, CUserCmd* cmd);
+	extern void __stdcall PaintTraverseHook(int vguiID, bool force, bool allowForcing);
 }
