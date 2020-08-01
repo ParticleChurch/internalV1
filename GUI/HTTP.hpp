514cbe199c3e2f58d5b55287e088254aeaf3fe3e
unknown <gabe.millikan@geoffandcindy.com>
2020-07-31 20:03:51 -0700

    Add new HTTP methods

---
+++ GUI/HTTP.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <windows.h>
+#include <wininet.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#pragma comment(lib, "wininet.lib")
+
+namespace HTTP {
+    extern std::string URLEncode(const std::string& value);
+
+    struct Arguments {
+        unsigned int Count = 0;
+        std::string String = "";
+
+        void Add(std::string Key, std::string Value)
+        {
+            if (this->Count > 0)
+                this->String += "&";
+            this->String += URLEncode(Key) + "=" + URLEncode(Value);
+
+            this->Count++;
+        }
+    };
+
+    extern std::string POST(std::string Host, std::string Directory, std::string URLArguments);
+    extern std::string API(std::string CMD, Arguments Arguments);
+}
