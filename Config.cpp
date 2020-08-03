c68ef3b5918ed7fc914fea7d5f55768bfe981783
[Graham Donaldson] <[grahambo437@gmail.com]>
2020-08-02 19:51:29 -0700

    added configs... though any color doesn't work...

--- Config.cpp
+++ Config.cpp
@@ -1,3 +1,12 @@
 #include "Include.hpp"
 
 UserInfoStruct Config::UserInfo = UserInfoStruct{};
+
+namespace Config
+{
+	Config_LegitBot legitbot;
+	Config_RageBot ragebot;
+	Config_AntiAim antiaim;
+	Config_Movement movement;
+	Config_Visuals visuals;
+}
